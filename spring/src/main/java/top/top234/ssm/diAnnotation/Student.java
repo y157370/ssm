168c6ef46ddfbcdee1bf3f1d5f1ca2e03a20eedf
y157370 <y157370@163.com>
2021-03-17 19:46:13 +0800

    spring ioc di依赖注入

--- spring/src/main/java/top/top234/ssm/diAnnotation/Student.java
+++ spring/src/main/java/top/top234/ssm/diAnnotation/Student.java
@@ -5,8 +5,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.Resource;
-
 /**
  * Don't forget to be awesome!
  *
